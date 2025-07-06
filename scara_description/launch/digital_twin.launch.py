91cd1cc7d67724241fd6ed2e6e44b4cfe4508bba
seungpyo <skg57605@gmail.com>
2025-07-06 22:48:51 +0900

    digitaltwin 실행 불가 오류 해결중

--- scara_description/launch/digital_twin.launch.py
+++ scara_description/launch/digital_twin.launch.py
@@ -1,5 +1,3 @@
-# ~/ros2_ws/src/scara_description/launch/digital_twin.launch.py
-
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
@@ -8,32 +6,40 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 
 def generate_launch_description():
-
     scara_description_pkg = get_package_share_directory('scara_description')
     arduino_interface_pkg = get_package_share_directory('arduino_interface')
-
-    # 1. Gazebo 시뮬레이션 실행 (시뮬레이션 '몸')
+    
+    # 1. Gazebo 시뮬레이션 환경 실행
     start_gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(scara_description_pkg, 'launch', 'gazebo.launch.py')
         )
     )
 
-    # 2. IMU 인터페이스 실행 (센서 입력부)
+    # 2. 실제 로봇 하드웨어 드라이버 실행
+    dynamixel_info_yaml_path = os.path.join(scara_description_pkg, 'config', 'dynamixel_info.yaml')
+    scara_controllers_yaml_path = os.path.join(scara_description_pkg, 'config', 'scara_controllers.yaml')
+    
+    start_real_robot_driver = Node(
+        package='dynamixel_workbench_ros',
+        executable='dynamixel_workbench_ros',
+        name='dynamixel_workbench',
+        output='screen',
+        parameters=[
+            {'use_sim_time': True}, # 디지털 트윈이므로 시뮬레이션 시간에 맞춤
+            dynamixel_info_yaml_path,
+            scara_controllers_yaml_path
+        ]
+    )
+
+    # 3. IMU 인터페이스 실행
     start_imu_interface = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(arduino_interface_pkg, 'launch', 'start_imu.launch.py')
         )
     )
     
-    # 3. 실제 로봇 하드웨어 드라이버 실행 (실제 로봇 '몸')
-    start_real_robot = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(scara_description_pkg, 'launch', 'real_robot.launch.py')
-        )
-    )
-
-    # 4. IK 제어 노드 실행 (두뇌)
+    # 4. IK 제어 노드 실행 ('두뇌')
     start_ik_node = Node(
         package='scara_control',
         executable='scara_ik_node',
@@ -41,9 +47,18 @@ def generate_launch_description():
         output='screen'
     )
 
+    # 5. 디지털 트윈을 위한 '그림자' 노드 실행
+    start_shadow_node = Node(
+        package='scara_control',
+        executable='shadow_controller_node',
+        name='shadow_controller_node',
+        output='screen'
+    )
+
     return LaunchDescription([
         start_gazebo,
+        start_real_robot_driver,
         start_imu_interface,
-        # start_real_robot, # 실제 로봇을 연결했을 때 이 줄의 주석을 해제하세요!
-        start_ik_node
+        start_ik_node,
+        start_shadow_node
     ])
