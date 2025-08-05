48951c745fe8d89129140b98f3b9faebf4b54efb
seungpyo <skg57605@gmail.com>
2025-08-05 13:05:02 +0900

    fix

--- Arduino_to_ros_final/Arduino_to_ros_final.ino
+++ Arduino_to_ros_final/Arduino_to_ros_final.ino
@@ -6,31 +6,6 @@
 
 MPU9250_asukiaaa mySensor;
 
-// =======================================================================
-// ===> 1단계에서 정리한 최종 보정값들을 여기에 붙여넣으세요! <===
-// =======================================================================
-// 자이로 바이어스 (단위: dps - 도/초)
-const float G_B_X_dps = -0.0128 * 180.0 / M_PI;
-const float G_B_Y_dps = 0.1062 * 180.0 / M_PI;
-const float G_B_Z_dps = -0.0139 * 180.0 / M_PI;
-
-// 가속도계 바이어스 (단위: g)
-const float A_B_X_g = -0.0553;
-const float A_B_Y_g = 0.0071;
-const float A_B_Z_g = 0.0511;
-
-// 가속도계 스케일
-const float A_S_X = -0.9979;
-const float A_S_Y = -1.0015;
-const float A_S_Z = -1.0145;
-
-// 지자기 바이어스 (단위: uT)
-const float M_B_X = 87.5650;
-const float M_B_Y = -115.5700;
-const float M_B_Z = -64.8850;
-// =======================================================================
-
-
 void setup() {
   Serial.begin(115200);
   while(!Serial) {}
@@ -41,8 +16,8 @@ void setup() {
   mySensor.beginAccel();
   mySensor.beginGyro();
   mySensor.beginMag();
-  
-  Serial.println("Calibrated MPU9250 is online and sending data.");
+  Serial.println("acc_ax, acc_ay, acc_az");
+  //Serial.println("Calibrated MPU9250 is online and sending data.");
 }
 // 최종 완성형 loop() 함수 - dtostrf 사용
 
@@ -53,27 +28,35 @@ void loop() {
   mySensor.magUpdate();
 
   // 값 계산 및 단위 변환 (기존과 동일)
-  float cal_ax = (mySensor.accelX() - A_B_X_g) / A_S_X;
-  float cal_ay = (mySensor.accelY() - A_B_Y_g) / A_S_Y;
-  float cal_az = (mySensor.accelZ() - A_B_Z_g) / A_S_Z;
-  float cal_gx_dps = mySensor.gyroX() - G_B_X_dps;
-  float cal_gy_dps = mySensor.gyroY() - G_B_Y_dps;
-  float cal_gz_dps = mySensor.gyroZ() - G_B_Z_dps;
-  float cal_mx = mySensor.magX() - M_B_X;
-  float cal_my = mySensor.magY() - M_B_Y;
-  float cal_mz = mySensor.magZ() - M_B_Z;
+  float cal_ax = mySensor.accelX();
+  float cal_ay = mySensor.accelY();
+  float cal_az = mySensor.accelZ();
+  float cal_gx_dps = mySensor.gyroX();
+  float cal_gy_dps = mySensor.gyroY();
+  float cal_gz_dps = mySensor.gyroZ();
+  float cal_mx = mySensor.magX();
+  float cal_my = mySensor.magY();
+  float cal_mz = mySensor.magZ();
   
   const float g_to_mss = 9.80665;
   const float dps_to_rads = M_PI / 180.0;
   
-  float final_ax = cal_ax * g_to_mss;
-  float final_ay = cal_ay * g_to_mss;
-  float final_az = cal_az * g_to_mss;
+  float final_ax = (cal_ax * g_to_mss)+0.4;
+  float final_ay = (cal_ay * g_to_mss)+0.01;
+  float final_az = (cal_az * g_to_mss)-0.4;
   float final_gx_rads = cal_gx_dps * dps_to_rads;
   float final_gy_rads = cal_gy_dps * dps_to_rads;
   float final_gz_rads = cal_gz_dps * dps_to_rads;
+  //Serial.print("acc_ax:");
+  Serial.print(cal_ax);
+  Serial.print(", ");
+  //Serial.print("acc_ay:");
+  Serial.print(cal_ay);
+  Serial.print(", ");
+  //Serial.print("acc_az:");
+  Serial.println(cal_az);
 
-  // ====================================================================
+  /*// ====================================================================
   // ===> 핵심 수정 사항: dtostrf()를 사용해 데이터를 문자열로 변환 <===
   // ====================================================================
   
@@ -105,7 +88,7 @@ void loop() {
 
   // 4. 완성된 문장을 한 번에 전송합니다.
   Serial.println(tx_buffer);
-  // ====================================================================
+  // ====================================================================*/
 
   delay(20);
 }
